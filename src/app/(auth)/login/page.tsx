a48af3113e7a679130b06504e139efdda0743380
ThiagoR19 <riffothiago19@gmail.com>
2026-08-07 16:52:01 -0300

    feat: add new logo and center login-form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from '@/components/login-form'
 
 export default function LoginPage() {
   return (
-    <main className='h-screen'>
+    <main className='flex h-screen justify-center items-center'>
       <LoginForm />
     </main>
   )
