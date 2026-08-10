8c455decdd3da18fbe07180de05a80902ec992a0
ThiagoR19 <riffothiago19@gmail.com>
2026-08-10 20:27:00 -0300

    feat: add sidebar in dashboard pages

--- src/app/(dashboard)/clients/page.tsx
+++ src/app/(dashboard)/clients/page.tsx
@@ -1,5 +1,5 @@
 export default function Clients() {
   return (
-    <div>Clients</div>
+    <section>Para bailar la bamba se necesita una bomba de grasa (Clients)</section>
   )
 }
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,15 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarProvider } from "@/components/sidebar"
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>
+  return (
+    <main className="flex">
+      <SidebarProvider>
+        <AppSidebar />
+      </SidebarProvider>
+      {children}
+    </main>
+  )
 }
 
 
